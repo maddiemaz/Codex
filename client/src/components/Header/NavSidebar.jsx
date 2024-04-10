e7898ecc54171c15d1ed4ec883e140e23af2172c
maddiemaz <maddiemaz18@gmail.com>
2024-04-10 12:01:28 -0400

    Fixed alignment of NavBar links

--- client/src/components/Header/NavSidebar.jsx
+++ client/src/components/Header/NavSidebar.jsx
@@ -10,21 +10,21 @@ export default function NavSidebar({toggleNavBar}) {
             </div>
             <div className="navbar-links">
                 <div className="navbar-links-owned">
-                    <div className="text-title-18">Your Codex</div>
+                    <div className="text-title-22">Your Codex</div>
                     <div className="navbar-links-owned-links"> 
                     {/* Add Links; Toggle Off if not logged in */}
-                        <div className="text-title-14 text-links-navbar-owned" onClick={toggleNavBar}>Worlds</div>
-                        <div className="text-title-14 text-links-navbar-owned" onClick={toggleNavBar}>Works</div>
-                        <div className="text-title-14 text-links-navbar-owned" onClick={toggleNavBar}>Characters</div>
+                        <div className="text-body-20 text-links-navbar-owned" onClick={toggleNavBar}>Worlds</div>
+                        <div className="text-body-20 text-links-navbar-owned" onClick={toggleNavBar}>Works</div>
+                        <div className="text-body-20 text-links-navbar-owned" onClick={toggleNavBar}>Characters</div>
                     </div>
                 </div>
                 <div className="navbar-links-all">
-                    <div className="text-title-18">All Codices</div>
+                    <div className="text-title-22">All Codices</div>
                     <div className="navbar-links-all-links"> 
-                        <Link to="/worlds" className="text-title-14 text-links-navbar-all" onClick={toggleNavBar}>Worlds</Link>
-                        {/* <Link to="/works" className="text-title-14 text-links-navbar-all" onClick={toggleNavBar}>All Works</Link> */}
-                        <div className="text-title-14 text-links-navbar-all">Works</div>
-                        <Link to="/characters" className="text-title-14 text-links-navbar-all" onClick={toggleNavBar}>Characters</Link>
+                        <Link to="/worlds" className="text-body-20 text-links-navbar-all" onClick={toggleNavBar}>Worlds</Link>
+                        {/* <Link to="/works" className="text-body-20 text-links-navbar-all" onClick={toggleNavBar}>All Works</Link> */}
+                        <Link className="text-body-20 text-links-navbar-all">Works</Link>
+                        <Link to="/characters" className="text-body-20 text-links-navbar-all" onClick={toggleNavBar}>Characters</Link>
                     </div>
                 </div>
             </div>
