f344b66fc86a687e354de9aeb6e9d54e710ff006
maddiemaz <maddiemaz18@gmail.com>
2024-04-11 10:59:40 -0400

    Home Page

--- client/src/components/Main/Home.jsx
+++ client/src/components/Main/Home.jsx
@@ -1,3 +1,15 @@
 export default function Home () {
     
+    return (
+        <div className="home-page">
+            <div className="home-content-container">
+                <div className="home-graphic-container">
+                    <div className="placeholder-graphic">codex</div>
+                </div>
+                <div className="welcome-text-container">
+                    <div className="text-body-18">Welcome to Codex, the ultimate index for organizing your creative projects!</div>
+                </div>
+            </div>
+        </div>
+    )
 }
